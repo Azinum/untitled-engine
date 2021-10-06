e95403c8a254004b38472924af436d5a3c672eca
azinum <lukaska61@gmail.com>
2021-10-06 20:21:12 +0200

    Add sdbm hash function

--- include/hash.h
+++ include/hash.h
@@ -7,4 +7,6 @@ typedef u64 hash_t;
 
 hash_t djb2_hash(u8* data, u32 size);
 
+hash_t sdbm_hash(u8* data, u32 size);
+
 #endif
--- src/hash.c
+++ src/hash.c
@@ -10,3 +10,14 @@ hash_t djb2_hash(u8* data, u32 size) {
   }
   return result;
 }
+
+hash_t sdbm_hash(u8* data, u32 size) {
+  hash_t result = 0;
+  if (!data) {
+    return result;
+  }
+  for (u32 i = 0; i < size; ++i) {
+    result = data[i] + (result << 6) + (result << 16) - result;
+  }
+  return result;
+}
